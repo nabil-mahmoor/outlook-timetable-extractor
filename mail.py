c4a3b4d03dfb9eb8c621611315a168fbfd5f055b
Nabil <nabil.mahmoor@gmail.com>
2026-03-23 15:46:48 +0530

    Implement pdf download

--- mail.py
+++ mail.py
@@ -29,3 +29,34 @@ def get_latest_timetable_email(token)-> dict | None:
     messages.sort(key=lambda m: m["receivedDateTime"], reverse=True)
     
     return messages[0]
+
+def get_pdf_attachment(token, message_id):
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    
+    response = requests.get(
+        f"{GRAPH_BASE_URL}/me/messages/{message_id}/attachments",
+        headers=headers
+    )
+    
+    response.raise_for_status()
+    attachments = response.json().get("value", [])
+
+    for attachment in attachments:
+        if attachment.get("contentType") == "application/pdf":
+            return attachment
+        
+    return None
+
+def download_pdf(attachment, timestamp=""):
+    import base64
+    
+    download_path = f"timetable{"_" + timestamp}.pdf"
+    
+    pdf_data = base64.b64decode(attachment.get("contentBytes"))
+
+    with open(download_path, "wb") as f:
+        f.write(pdf_data)
+    
+    print(f"PDF downloaded to {download_path}")
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+from datetime import datetime
 from auth import get_access_token
-from mail import get_latest_timetable_email
+from mail import get_latest_timetable_email, get_pdf_attachment, download_pdf
 
 def main():
     try:
@@ -7,10 +8,20 @@ def main():
         print("Access token aquired successfully")
         
         email = get_latest_timetable_email(token)
-        if email:
-            print(f"Found email: '{email.get('subject')}' received on {email.get('receivedDateTime')}")
-        else:
+
+        if not email:
             print("No timetable email found.")
+        else:
+            timestamp = datetime.strptime(email.get('receivedDateTime'), "%Y-%m-%dT%H:%M:%SZ")
+            print(f"Found email: '{email.get('subject')}' received on {timestamp}")
+
+            attachment = get_pdf_attachment(token, email.get("id"))
+
+            if not attachment:
+                print("No PDF attachment found in email.")
+            else:
+                date = str(timestamp).split(" ")[0]
+                download_pdf(attachment, date)
 
     except Exception as e:
         print(f"Something went wrong: {e}")
